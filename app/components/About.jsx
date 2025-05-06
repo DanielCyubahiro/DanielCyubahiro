e84736d2f7a694de4194452523e120d2a101b419
Daniel <cyubahirodaniel@gmail.com>
2025-05-06 15:51:27 +0200

    Update layout

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,95 +1,102 @@
 import Image from 'next/image';
+
 const About = () => {
   return (
-      <section id={'about'} className={'w-full px-[12%] py-10 scroll-mt-20 flex flex-col items-center justify-center'}>
+      <section id={'about'}
+               className={'w-full px-[12%] py-10 scroll-mt-20 flex flex-col items-center justify-center'}>
         <h2 className="text-center text-5xl mb-20">
           About Me
         </h2>
-        <p className={'mb-10 max-w-2xl text-justify'}>
-          I was born and raised in Rwanda, and moved to Italy where I earned
-          a degree in Computer Engineering.
-          My professional growth continued in Italy for over several years,
-          during which I had the privilege of contributing to the success of
-          several large-scale projects across diverse
-          sectors, including tech, telecommunications, and healthcare.
-          <br/>
-          I've recently relocated to Berlin and I'm eager to build on this
-          experience.
-        </p>
-        <div>
-          <h4 className="text-center mb-2 text-2xl">Tech Stack</h4>
-          <h5 className={'text-center m-5'}>🖥️ Frontend</h5>
-          <section className={'flex gap-2 flex-wrap justify-center'}>
-            <Image src={'/icons/html.svg'} width={40} height={40}
-                   alt={'html logo'}/>
-            <Image src={'/icons/css.svg'} width={40} height={40}
-                   alt={'css logo'}/>
-            <Image src={'/icons/javascript.svg'} width={40} height={40}
-                   alt={'javascript logo'}/>
-            <Image src={'/icons/typescript.svg'} width={40} height={40}
-                   alt={'typescript logo'}/>
-            <Image src={`/icons/react-dark.svg`} width={40} height={40}
-                   alt={'react logo'}/>
-            <Image src={`/icons/nextjs-dark.svg`} width={40} height={40}
-                   alt={'nextjs logo'}/>
-            <Image src={`/icons/vite-dark.svg`} width={40} height={40}
-                   alt={'vite logo'}/>
-            <Image src={'/icons/redux.svg'} width={40} height={40}
-                   alt={'redux logo'}/>
-            <Image src={`/icons/tailwindcss-dark.svg`} width={40}
-                   height={40} alt={'tailwindcss logo'}/>
-            <Image src={`/icons/materialui-dark.svg`} width={40}
-                   height={40} alt={'tailwindcss logo'}/>
-          </section>
-          <h5 className={'text-center m-5'}>⚙️ Backend</h5>
-          <section className={'flex gap-2 flex-wrap justify-center'}>
-            <Image src={`/icons/nodejs-dark.svg`} width={40} height={40}
-                   alt={'nodejs logo'}/>
-            <Image src={`/icons/php-dark.svg`} width={40} height={40}
-                   alt={'php logo'}/>
-            <Image src={`/icons/expressjs-dark.svg`} width={40}
-                   height={40} alt={'expressjs logo'}/>
-            <Image src={`/icons/nestjs-dark.svg`} width={40} height={40}
-                   alt={'nestjs logo'}/>
-            <Image src={`/icons/laravel-dark.svg`} width={40}
-                   height={40}
-                   alt={'laravel logo'}/>
-          </section>
-          <h5 className={'text-center m-5'}>🗄️️ Database</h5>
-          <section className={'flex gap-2 flex-wrap justify-center'}>
-            <Image src={'/icons/mongodb.svg'} width={40} height={40}
-                   alt={'mongodb logo'}/>
-            <Image src={`/icons/mysql-dark.svg`} width={40} height={40}
-                   alt={'mysql logo'}/>
-            <Image src={`/icons/postgresql-dark.svg`} width={40}
-                   height={40}
-                   alt={'postgresql logo'}/>
-          </section>
-          <h5 className={'text-center m-5'}>️🛠 Other Tools</h5>
-          <section className={'flex gap-2 flex-wrap justify-center'}>
-            <Image src={'/icons/git.svg'} width={40} height={40}
-                   alt={'git logo'}/>
-            <Image src={`/icons/github-dark.svg`} width={40} height={40}
-                   alt={'github logo'}/>
-            <Image src={`/icons/netlify-dark.svg`} width={40}
-                   height={40}
-                   alt={'netlify logo'}/>
-            <Image src={`/icons/vercel-dark.svg`} width={40}
-                   height={40}
-                   alt={'vercel logo'}/>
-            <Image src={`/icons/npm-dark.svg`} width={40}
-                   height={40}
-                   alt={'npm logo'}/>
-            <Image src={'/icons/postman.svg'} width={40} height={40}
-                   alt={'postman logo'}/>
-            <Image src={`/icons/webstorm-dark.svg`} width={40}
-                   height={40}
-                   alt={'webstorm logo'}/>
+        <div className="lg:flex justify-between gap-5">
+          <div className="flex flex-col lg:w-[50%] justify-center">
+            <h4 className="mb-2 text-2xl text-center">Background</h4>
+            <p className={'mb-10 max-w-2xl'}>
+              I was born and raised in Rwanda, and moved to Italy where I earned
+              a degree in Computer Engineering.
+              My professional growth continued in Italy for over several years,
+              during which I had the privilege of contributing to the success of
+              several large-scale projects across diverse
+              sectors, including tech, telecommunications, and healthcare.
+              <br/>
+              I've recently relocated to Berlin and I'm eager to build on this
+              experience.
+            </p>
+          </div>
+          <div className="lg:w-[50%]">
+            <h4 className="text-center mb-2 text-2xl">Tech Stack</h4>
+            <h5 className={'text-center m-5'}>🖥️ Frontend</h5>
+            <section className={'flex gap-2 flex-wrap justify-center'}>
+              <Image src={'/icons/html.svg'} width={40} height={40}
+                     alt={'html logo'}/>
+              <Image src={'/icons/css.svg'} width={40} height={40}
+                     alt={'css logo'}/>
+              <Image src={'/icons/javascript.svg'} width={40} height={40}
+                     alt={'javascript logo'}/>
+              <Image src={'/icons/typescript.svg'} width={40} height={40}
+                     alt={'typescript logo'}/>
+              <Image src={`/icons/react-dark.svg`} width={40} height={40}
+                     alt={'react logo'}/>
+              <Image src={`/icons/nextjs-dark.svg`} width={40} height={40}
+                     alt={'nextjs logo'}/>
+              <Image src={`/icons/vite-dark.svg`} width={40} height={40}
+                     alt={'vite logo'}/>
+              <Image src={'/icons/redux.svg'} width={40} height={40}
+                     alt={'redux logo'}/>
+              <Image src={`/icons/tailwindcss-dark.svg`} width={40}
+                     height={40} alt={'tailwindcss logo'}/>
+              <Image src={`/icons/materialui-dark.svg`} width={40}
+                     height={40} alt={'tailwindcss logo'}/>
+            </section>
+            <h5 className={'text-center m-5'}>⚙️ Backend</h5>
+            <section className={'flex gap-2 flex-wrap justify-center'}>
+              <Image src={`/icons/nodejs-dark.svg`} width={40} height={40}
+                     alt={'nodejs logo'}/>
+              <Image src={`/icons/php-dark.svg`} width={40} height={40}
+                     alt={'php logo'}/>
+              <Image src={`/icons/expressjs-dark.svg`} width={40}
+                     height={40} alt={'expressjs logo'}/>
+              <Image src={`/icons/nestjs-dark.svg`} width={40} height={40}
+                     alt={'nestjs logo'}/>
+              <Image src={`/icons/laravel-dark.svg`} width={40}
+                     height={40}
+                     alt={'laravel logo'}/>
+            </section>
+            <h5 className={'text-center m-5'}>🗄️️ Database</h5>
+            <section className={'flex gap-2 flex-wrap justify-center'}>
+              <Image src={'/icons/mongodb.svg'} width={40} height={40}
+                     alt={'mongodb logo'}/>
+              <Image src={`/icons/mysql-dark.svg`} width={40} height={40}
+                     alt={'mysql logo'}/>
+              <Image src={`/icons/postgresql-dark.svg`} width={40}
+                     height={40}
+                     alt={'postgresql logo'}/>
+            </section>
+            <h5 className={'text-center m-5'}>️🛠 Other Tools</h5>
+            <section className={'flex gap-2 flex-wrap justify-center'}>
+              <Image src={'/icons/git.svg'} width={40} height={40}
+                     alt={'git logo'}/>
+              <Image src={`/icons/github-dark.svg`} width={40} height={40}
+                     alt={'github logo'}/>
+              <Image src={`/icons/netlify-dark.svg`} width={40}
+                     height={40}
+                     alt={'netlify logo'}/>
+              <Image src={`/icons/vercel-dark.svg`} width={40}
+                     height={40}
+                     alt={'vercel logo'}/>
+              <Image src={`/icons/npm-dark.svg`} width={40}
+                     height={40}
+                     alt={'npm logo'}/>
+              <Image src={'/icons/postman.svg'} width={40} height={40}
+                     alt={'postman logo'}/>
+              <Image src={`/icons/webstorm-dark.svg`} width={40}
+                     height={40}
+                     alt={'webstorm logo'}/>
 
-            <Image src={`/icons/vscode-dark.svg`} width={40}
-                   height={40}
-                   alt={'vscode logo'}/>
-          </section>
+              <Image src={`/icons/vscode-dark.svg`} width={40}
+                     height={40}
+                     alt={'vscode logo'}/>
+            </section>
+          </div>
         </div>
       </section>
   );
